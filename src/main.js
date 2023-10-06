c7071143729423a0963dec4aaa7c2d44c0d6f5e5
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2023-10-06 14:22:09 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]')
-    const tabsContainer =   document.querySelectorAll('[data-tab-id]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
-    for (let i = 0; 1 < buttons.length; i++) {
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader()
+        } else {
+            exibeElementosDoHeader()
+        }
+    })
+
+
+    // Seção de atrações. programação das abas
+    for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
@@ -11,6 +26,30 @@ document.addEventListener('DOMContentLoaded', function() {
             removeBotaoAtivo()
             botao.target.classList.add('shows__tabs__button--is-active')
         })
+    }
+
+
+    //Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+
+    }
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}   
+
+function exibeElementosDoHeaderElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}  
+
+    function abreOuFechaResposta(elemento) {
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = elemento.target.parentNode
+
+        elementoPai.classList.toggle(classe)
     }
 
     function removeBotaoAtivo() {
